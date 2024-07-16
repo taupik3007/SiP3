4bd20794141487b8d892008a68d0d1180c272fa9
taupik3007 <taupikpathurrohman@gmail.com>
2024-07-16 23:28:13 +0700

    menambah seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $kesiswaan = User::create([
+            'name'      => "kesiswaan",
+            'email'     =>  "kesiswaan@gmail.com",
+            'password'  =>  bcrypt('kesiswaan12312311'),
+           
+        ]);
+        $kesiswaan->assignRole('kesiswaan');
+    }
+}
